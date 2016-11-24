4416115968a996bd2eb3786e3e0fdcee4a737193
Mickey <ekey.huang@gmail.com>
2016-11-24 03:24:15 -0500

    updated

--- Blog_Generator.py
+++ Blog_Generator.py
@@ -1,18 +1,19 @@
 import os
 
 
-def sysc_git(blog_path):
+def sync_git(blog_path):
     os.chdir(blog_path)
     os.system("git status; git pull")
 
 
+# create and auto open the blog
 def creat_new_blog(blog_path, blog_file):
     print "\nGenerating the blog......"
     os.system("cp /Users/MICKEY/Github/BlogGenerator/templates/default.html " +
               blog_path + "/article/" + blog_file)
     os.chdir(blog_path + "/article")
-    os.system("atom " + blog_file)
     print "\nNew blog has been created, please edit it in Atom.\n"
+    os.system("atom " + blog_file)
 
 
 def main():
@@ -22,13 +23,16 @@ def main():
                      " ? if not, Enter \"N\",if yes, just pass:\n")
     if path.upper() == "N":
         blog_path = raw_input("Please enter your blog path:\n")
-    sysc_git(blog_path)
+
+    sync_git(blog_path)
+
     new_blog = raw_input("\nDo you want to start a new blog?(Y/N)\n")
     if new_blog.upper() == "Y":
         blog_file = raw_input(
             "\nwhat is the file of your new blog?\n") + ".html"
         if blog_file == ".html":
             blog_file = "new_blog.html"
+
     creat_new_blog(blog_path, blog_file)
 
 
