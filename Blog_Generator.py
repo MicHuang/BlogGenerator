920aad9e7e353bafcabb5d1bbab9208d5420accf
Mickey <ekey.huang@gmail.com>
2016-11-24 02:57:13 -0500

    add main function

--- Blog_Generator.py
+++ Blog_Generator.py
@@ -1,24 +1,33 @@
 import os
 
-print "\nWelcome to the super simple Blog Generator v1.0\n"
-blog_path = "~/Github/michuang.github.io"
-path = raw_input(
-    "Is your bolg in: ~/Github/michuang.github.io ? if not, Enter \"N\",if yes, just pass:\n")
 
-if path.upper() == "N":
-    blog_path = raw_input("Please enter your blog path:\n")
+def sysc_git(blog_path):
+    os.chdir(blog_path)
+    os.system("git status; git pull")
 
-os.system("cd " + blog_path + "; git status")
-os.system("cd " + blog_path + "; git pull")
 
-new_blog = raw_input("\nDo you want to start a new blog?(Y/N)\n")
-if new_blog.upper() == "Y":
-    blog_name = raw_input("\nwhat is the name of your new blog?\n")
-    if blog_name == "":
-        blog_name = "new_blog"
-
+def creat_new_blog(blog_path, blog_name):
     print "\nGenerating the blog......"
-    os.system("cp templates/default.html " + blog_path + "/article/" +
-              blog_name + ".html")
+    os.system("cp /Users/MICKEY/Github/BlogGenerator/templates/default.html " +
+              blog_path + "/article/" + blog_name + ".html")
     os.system("cd " + blog_path + "/article ; atom " + blog_name + ".html")
     print "\nNew blog has been created, please edit it in Atom.\n"
+
+
+def main():
+    print "\nWelcome to the super simple Blog Generator v1.0\n"
+    blog_path = "/Users/MICKEY/Github/michuang.github.io"
+    path = raw_input("Is your bolg in: " + blog_path +
+                     " ? if not, Enter \"N\",if yes, just pass:\n")
+    if path.upper() == "N":
+        blog_path = raw_input("Please enter your blog path:\n")
+    sysc_git(blog_path)
+    new_blog = raw_input("\nDo you want to start a new blog?(Y/N)\n")
+    if new_blog.upper() == "Y":
+        blog_name = raw_input("\nwhat is the name of your new blog?\n")
+        if blog_name == "":
+            blog_name = "new_blog"
+    creat_new_blog(blog_path, blog_name)
+
+
+main()
